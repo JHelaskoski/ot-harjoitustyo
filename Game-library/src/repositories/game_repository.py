a75e2432bfe557003e86978c8f381d948d984e42
JHelaskoski <janicaanna1@gmail.com>
2026-03-30 10:58:07 +0300

    Lisätty muutama toiminnallisuus game repoon

--- Game-library/src/repositories/game_repository.py
+++ Game-library/src/repositories/game_repository.py
@@ -3,3 +3,19 @@ from database_connection import get_database_connection
 class GameRepository:
     def __init__(self):
         self._connection = get_database_connection()
+        
+    def add_game(self, name, console_model_id, release_year, status):
+        cursor = self._connection.cursor()
+        cursor.execute("""
+            insert into games (name, console_model_id, release_year, status) 
+            values (?, ?, ?, ?)
+        """, (name, console_model_id, release_year, status))
+        self._connection.commit()
+        
+    def get_all_games(self):
+        cursor = self._connection.cursor()
+        cursor.execute("""
+            select * from games
+        """)
+        return cursor.fetchall()
+    
