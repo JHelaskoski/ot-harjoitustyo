b80a3ec838e0d73d4e7565a6c48c7a9b218164d0
JHelaskoski <janicaanna1@gmail.com>
2026-03-30 14:09:48 +0300

    Alustettu tiedosto

---
+++ Game-library/src/entities/game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, name, release_year, status, console_model_id):
+        self.name = name
+        self.release_year = release_year
+        self.status = status
+        self.console_model_id = console_model_id
