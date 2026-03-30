74cd3b9c1cc92ff8d78448425f28855e5af9a200
JHelaskoski <janicaanna1@gmail.com>
2026-03-30 16:57:07 +0300

    Lisätty Invoke-tehtävät

---
+++ Game-library/tasks.py
@@ -0,0 +1,27 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+    
+@task    
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
+    
+#Windows käyttöön
+def start_w(ctx):
+    ctx.run("python3 src/index.py")
+
+@task
+def test_w(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage_report_w(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+    ctx.run("coverage html")
