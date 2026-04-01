56be70fc5b0fb45b0810aa62a2977d22c622eead
JHelaskoski <janicaanna1@gmail.com>
2026-04-01 13:25:14 +0300

    Lisätty oma käsky automaattiselle formatoinnille

--- Game-library/tasks.py
+++ Game-library/tasks.py
@@ -7,12 +7,12 @@ def start(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
-    
-@task    
+
+@task
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
-    
+
 #Windows käyttöön
 def start_w(ctx):
     ctx.run("python3 src/index.py")
@@ -25,3 +25,7 @@ def test_w(ctx):
 def coverage_report_w(ctx):
     ctx.run("coverage run --branch -m pytest src")
     ctx.run("coverage html")
+
+@task
+def format_w(ctx):
+    ctx.run("autopep8 --in-place --recursive src")
