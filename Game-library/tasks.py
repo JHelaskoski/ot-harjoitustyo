2d722d5e133da8828b4cc3fa094ffc16992d46a5
JHelaskoski <janicaanna1@gmail.com>
2026-04-19 17:26:24 +0300

    Täydennetty tasks

--- Game-library/tasks.py
+++ Game-library/tasks.py
@@ -5,6 +5,11 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+
+@task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
@@ -40,3 +45,7 @@ def format_w(ctx):
 @task
 def lint_w(ctx):
     ctx.run("pylint src")
+
+@task
+def build_w(ctx):
+    ctx.run("python3 src/build.py")
